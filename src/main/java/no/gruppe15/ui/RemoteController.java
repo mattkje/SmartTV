5a7efd667da8adfe417c7bd6e6a3db58c018b70e
Calm Matt <40216910+mattkje@users.noreply.github.com>
2023-10-26 12:05:19 +0200

    Reconnect functionality

--- src/main/java/no/gruppe15/ui/RemoteController.java
+++ src/main/java/no/gruppe15/ui/RemoteController.java
@@ -2,7 +2,10 @@ package no.gruppe15.ui;
 
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
+import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -10,12 +13,19 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.paint.Color;
 import javafx.util.Duration;
+import no.gruppe15.RemoteControl;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.SocketException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.Scanner;
+
+import static no.gruppe15.TvServer.PORT_NUMBER;
 
 /**
  * Controller class for the RemoteApp class.
@@ -69,7 +79,9 @@ public class RemoteController implements Initializable {
       try {
         serverResponse = socketReader.readLine();
       } catch (IOException e) {
-        throw new RuntimeException(e);
+        printWriter = null;
+        connection.setText("Connection lost");
+        connection.setTextFill(Color.YELLOW);
       }
       System.out.println(command);
       System.out.println("Server Response: " + serverResponse);
@@ -95,7 +107,7 @@ public class RemoteController implements Initializable {
     sendCommandToServer("c4");
   }
   public void mute() {
-    sendCommandToServer("m");
+    sendCommandToServer("exit");
   }
 
   public void getNumberOfChannels() {
@@ -104,12 +116,64 @@ public class RemoteController implements Initializable {
 
   @Override
   public void initialize(URL url, ResourceBundle resourceBundle) {
-    if (printWriter != null){
+    // Create a Timeline that updates the connection status every N milliseconds
+    Timeline timeline = new Timeline(new KeyFrame(
+            Duration.millis(1000), // Update every 1 second, change this as needed
+            event -> updateConnectionStatus()));
+    timeline.setCycleCount(Timeline.INDEFINITE); // Run indefinitely
+    timeline.play();
+  }
+
+  private void updateConnectionStatus() {
+    if (printWriter != null) {
       connection.setText("Connected");
       connection.setTextFill(Color.LIME);
     } else {
       connection.setText("Not connected");
       connection.setTextFill(Color.RED);
     }
+
   }
+
+  public void reConnect() {
+    Platform.runLater(() -> {
+      connection.setTextFill(Color.YELLOW);
+      connection.setText("Connecting...");
+    });
+    new Thread(() -> {
+      try {
+        Socket socket = new Socket("localhost", PORT_NUMBER);
+        printWriter = new PrintWriter(socket.getOutputStream(), true);
+        socketReader = new BufferedReader(
+                new InputStreamReader(socket.getInputStream()));
+
+        Scanner userInputScanner = new Scanner(System.in);
+        boolean exit = false;
+
+        while (!exit) {
+          System.out.print("Enter a message: ");
+          String input = userInputScanner.nextLine();
+
+          printWriter.println(input);
+
+          if (input.equals("exit")) {
+            exit = true;
+          }
+
+          // TODO: ADD exception handling if there is a wrong command
+          String serverResponse = socketReader.readLine();
+          System.out.println("Server Response: " + serverResponse);
+        }
+
+        printWriter.close();
+        socketReader.close();
+        socket.close();
+
+      } catch (IOException e) {
+        System.err.println("Could not establish connection to the server: " + e.getMessage());
+      }
+
+    }).start();
+  }
+
 }
